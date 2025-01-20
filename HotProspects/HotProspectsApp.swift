81217462770c46f8cab3c6be4f3f44f362013723
Saydulayev <saydulayev.wien@gmail.com>
2025-01-20 10:04:17 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Saydulayev on 20.01.25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
