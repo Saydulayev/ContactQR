5dee6996522111cc42152efa23c67051ea18be3c
Saydulayev <saydulayev.wien@gmail.com>
2025-01-22 13:20:59 +0100

    feat: Add QR code generation, scaling, and scanning functionality
    - Implemented QR code generation using Core Image with support for scaling up images without blurring.
    - Added user input fields for name and email with @AppStorage integration for persistent storage.
    - Enhanced text fields with `textContentType` for better iOS autocomplete support.
    - Introduced image interpolation disabling for crisp QR code rendering.
    - Integrated CodeScanner package for seamless QR code scanning:
    - Added the CodeScanner library via Swift Package Manager from https://github.com/twostraws/CodeScanner.
    - Configured `CodeScannerView` for QR code scanning, including simulated data for testing in the simulator.
    - Implemented permission request for camera usage with a user-friendly message.
    - Added handling for scanned QR code data to extract name and email and store in the app's model.
    - Included fallback error handling for QR code generation and scanning failures.

---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,85 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Saydulayev on 22.01.25.
+//
+
+import SwiftData
+import SwiftUI
+import CodeScanner
+
+struct ProspectsView: View {
+    @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @Environment(\.modelContext) var modelContext
+    @State private var isShowingScanner = false
+
+    
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            "Everyone"
+        case .contacted:
+            "Contacted people"
+        case .uncontacted:
+            "Uncontacted people"
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
+                }
+            }
+                .navigationTitle(title)
+                .toolbar {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
+                    }
+                }
+                .sheet(isPresented: $isShowingScanner) {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Ahmad\nsaydulayev.wien@gmail.com", completion: handleScan)
+                }
+        }
+    }
+    init(filter: FilterType) {
+        self.filter = filter
+
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    func handleScan(result: Result<ScanResult, ScanError>) {
+       isShowingScanner = false
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+
+            modelContext.insert(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+}
+
+#Preview {
+    ProspectsView(filter: .none)
+        .modelContainer(for: Prospect.self)
+}
